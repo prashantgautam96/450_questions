2b7bf7d2a6f679039f2090d0c509725b5548922a
prashantgautam96 <92217129+prashantgautam96@users.noreply.github.com>
2022-06-12 11:22:32 +0530

    Create seperate_negPos.cpp

---
+++ seperate_negPos.cpp
@@ -0,0 +1,20 @@
+// 1) first aprocah will be sort the array 
+// 2)this approach
+
+
+#include <bits/stdc++.h>
+vector<int> separateNegativeAndPositive(vector<int> &nums){
+
+    int n= nums.size();
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] < 0)
+        {
+            if (i != j)
+                swap(nums[i], nums[j]);
+            j++;
+        }
+    }
+    return nums;
+}
